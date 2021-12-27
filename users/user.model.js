f06816779ba10333ff1a7ed8eaf760b871779f53
Арина <arinych24@gmail.com>
2021-12-27 14:00:53 +0300

    MySQL API

---
+++ users/user.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        name: { type: DataTypes.STRING, allowNull: false },
+        email: { type: DataTypes.STRING, allowNull: false },
+        dateOfRegistration: {type: DataTypes.STRING, allowNull: false},
+        dateOfLastLogin: {type: DataTypes.STRING, allowNull: false},
+        status: {type: DataTypes.STRING, allowNull: false},
+        hash: { type: DataTypes.STRING, allowNull: false }
+    };
+
+    const options = {
+        defaultScope: {
+            // exclude hash by default
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', attributes, options);
+}
---
+++ users/user.service.js
@@ -0,0 +1,91 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function authenticate({ name, password }) {
+    const user = await db.User.scope('withHash').findOne({ where: { name } });
+
+    if (!user || !(await bcrypt.compare(password, user.hash)))
+        throw 'name or password is incorrect';
+
+    user.dateOfLastLogin = new Date().toLocaleString();
+    await user.save();
+    // authentication successful
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+    return { ...omitHash(user.get()), token };
+}
+
+async function getAll() {
+    return await db.User.findAll();
+}
+
+async function getById(id) {
+    return await getUser(id);
+}
+
+async function create(params) {
+    // validate
+    if (await db.User.findOne({ where: { name: params.name } })) {
+        throw 'name "' + params.name + '" is already taken';
+    }
+
+    // hash password
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    params.dateOfRegistration = new Date().toLocaleString();
+    params.dateOfLastLogin = new Date().toLocaleString();
+    params.status = "Active";
+    // save user
+    await db.User.create(params);
+}
+
+async function update(id, params) {
+    const user = await getUser(id);
+
+    // validate
+    const nameChanged = params.name && user.name !== params.name;
+    if (nameChanged && await db.User.findOne({ where: { name: params.name } })) {
+        throw 'name "' + params.name + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    // copy params to user and save
+    Object.assign(user, params);
+    await user.save();
+
+    return omitHash(user.get());
+}
+
+async function _delete(id) {
+    const user = await getUser(id);
+    await user.destroy();
+}
+
+// helper functions
+
+async function getUser(id) {
+    const user = await db.User.findByPk(id);
+    if (!user) throw 'User not found';
+    return user;
+}
+
+function omitHash(user) {
+    const { hash, ...userWithoutHash } = user;
+    return userWithoutHash;
+}
